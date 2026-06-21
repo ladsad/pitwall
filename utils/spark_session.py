0ce609c862ef751c5fe4dacaeff2c8875ec7c388
ladsad <emailofshauryak@gmail.com>
2026-06-21 19:54:45 +0530

    fix: add HADOOP_HOME/bin to PATH to resolve NativeIO hadoop.dll access error

--- utils/spark_session.py
+++ utils/spark_session.py
@@ -11,7 +11,12 @@ os.environ["JAVA_HOME"] = os.path.join(conda_dir, "Library")
 
 # Set HADOOP_HOME for Windows compatibility (winutils.exe)
 project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-os.environ["HADOOP_HOME"] = os.path.join(project_dir, "hadoop")
+hadoop_home = os.path.join(project_dir, "hadoop")
+os.environ["HADOOP_HOME"] = hadoop_home
+
+# CRITICAL: add hadoop/bin to PATH so hadoop.dll can be loaded to fix NativeIO access0 error
+hadoop_bin = os.path.join(hadoop_home, "bin")
+os.environ["PATH"] = hadoop_bin + os.pathsep + os.environ.get("PATH", "")
 
 def _is_databricks() -> bool:
     return "DATABRICKS_RUNTIME_VERSION" in os.environ

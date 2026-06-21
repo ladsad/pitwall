9064180ca5f50edd398afee97290ae7a3681928f
ladsad <emailofshauryak@gmail.com>
2026-06-21 19:50:47 +0530

    fix: download winutils.exe and set HADOOP_HOME to fix NativeIO Windows access errors

--- utils/spark_session.py
+++ utils/spark_session.py
@@ -9,6 +9,10 @@ os.environ["PYSPARK_DRIVER_PYTHON"] = sys.executable
 conda_dir = os.path.dirname(sys.executable)
 os.environ["JAVA_HOME"] = os.path.join(conda_dir, "Library")
 
+# Set HADOOP_HOME for Windows compatibility (winutils.exe)
+project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+os.environ["HADOOP_HOME"] = os.path.join(project_dir, "hadoop")
+
 def _is_databricks() -> bool:
     return "DATABRICKS_RUNTIME_VERSION" in os.environ
 

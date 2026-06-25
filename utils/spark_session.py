e5ef8a2cd71bafa5315b0da937737983d553c598
ladsad <emailofshauryak@gmail.com>
2026-06-25 13:45:16 +0530

    Increase PySpark driver memory to 16GB

--- utils/spark_session.py
+++ utils/spark_session.py
@@ -23,5 +23,10 @@ def _is_databricks() -> bool:
     return "DATABRICKS_RUNTIME_VERSION" in os.environ
 
 def get_spark_session(app_name: str = "pitwall") -> SparkSession:
-    spark = SparkSession.builder.appName(app_name).getOrCreate()
+    spark = (
+        SparkSession.builder.appName(app_name)
+        .config("spark.driver.memory", "16g")
+        .config("spark.executor.memory", "4g")
+        .getOrCreate()
+    )
     return spark
